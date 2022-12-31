77a4dd316345ba3222e29e8c97613a5eaad02e34
Volzor <96282373+Volzor@users.noreply.github.com>
2022-12-30 19:07:17 -0800

    1-7 working
    still needs to pass some tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    let finalResult = 0;
+    const errorMessage = 'ERROR';
+    if (min < max) {
+        for (let i = min; i <= max; i++) {
+            finalResult += i;
+        }
+        return finalResult;
+    } else if (min > max) {
+        for (let i = max; i <= min; i++) {
+                finalResult += i;
+        }
+        return finalResult;
+    } else if (min < 0 || max < 0) {
+        for (let i = min; i <= max; i++) {
+            return errorMessage;
+        }
+    } else if (typeof min !== 'number' || typeof max !== 'number') {
+        return errorMessage;
+    };
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const conversion = (5/9) * (temp - 32);
+  return conversion;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const conversion = (temp * 2) + 30;
+  return conversion;
 };
 
 // Do not edit below this line
